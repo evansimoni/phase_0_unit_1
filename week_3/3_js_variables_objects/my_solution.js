ecee715742017bc3d9e98a7e279cbed7d9140e75
Evan Simoni <evan.simoni@gmail.com>
2014-05-28 23:22:07 -0700

    added my solution and reflection

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -1,24 +1,23 @@
-// I paired [by myself, with:] on this challenge.
+// I paired [by myself] on this challenge.
 
 // __________________________________________
 // Write your code below.
-
-
-
-
-
-
-
+var secretNumber = 7;
+var password = "just open the door";
+var allowedIn = false;
+var members = ['John', 'Joseph', 'Jesus', 'Mary'];
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
-// 
-// 
-// 
-// 
-// 
-// 
-
+// I approached this one with caution since a lot of the JS code was foreign to me, not
+// mention the concept of TDD. Even after working through this exercise I'm unsure of how I
+// would implement TDD into a project of my own. I don't understand how it could be used in
+// practice. My strategy for solving this challenge was to look at the test items one by one
+// and then write code that satified their requirements. I tested my code every time I modified
+// it, then went on to satify the next requirement. Some of my questions about how TTD work
+// were satisfied after practicing with and studying the code below. I feel like I satisfied
+// the learning competencies. Going through all the required reading definitely helped out in
+// understanding the these competencies.
 
 // __________________________________________
 // Test Code:  Do not alter code below this line.
