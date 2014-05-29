778e5e9444e162e437f8c6cf808771a0df781eee
Evan Simoni <evan.simoni@gmail.com>
2014-05-29 00:12:19 -0700

    added the user stories for the group project

---
+++ week_3/6_group_project/gc1_user_stories.js
@@ -0,0 +1,17 @@
+// My role in the group is:
+// Release 0, Release 2, Release 4
+
+// Here is my part of the challenge:
+// Release 0
+
+// tests for sum
+// The sum variable should be a function. It should be able to return the sum of the 
+// oddLengthArray (27) and the evenLengthArray (43).
+
+// tests for mean
+// The mean variable should be a function. It should be able to return the average of the
+// oddLengthArray (3.857142857142857) and the average of the evenLengthArray (5.375).
+
+// tests for median
+// The median variable should be a function. It should be able to return the median value of
+// the oddLengthArray (4) and the median value of the evenLengthArray (5.5).
