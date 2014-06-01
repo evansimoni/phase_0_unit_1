6e91305c9536e23b6d4ac498b3f0dfc112d31a32
Evan Simoni <evan.simoni@gmail.com>
2014-05-31 17:54:21 -0700

    partial solution

--- week_3/5_design_cc_mission_solo_challenge/my_solution.js
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -1,20 +1,53 @@
 // This is a solo challenge
 
 // Your mission description:
-// 
-// 
-// 
+
+// Objects: Knight, Princess, Ogre, Bridge, River
+
+// Objectives:
+// 1) Get across the bridge
+// 2) Get the princess to safety
+// 3) Cut down the bridge
+// 4) Drown the Ogre
+
+// My knight will get the attention of a nearby ogre who is harassing a princess. The knight tells the princess to follow
+//  him across a nearby bridge spanning a river. The ogre is slow but follows and is the last in the line crossing the bridge.
+// When the princess and the knight have crossed, the knight cuts down the bridge and the ogre falls into the water.
+
+// Methods
+// Yell (Knight)
+// Run Right (Knight and Princess)
+// Waddle Right (Ogre)
+// Chop/Attack (Knight)
+// Drown (Ogre)
 
 // Pseudocode
-// 
-// 
-// 
-// 
-// 
+
+// Knight Object
+// Running Speed 10
+// Health 20
+
+// Princess Object
+// Running Speed 9
+// Health 20
+
+// Ogre Object
+// Waddling Speed 5
+// Health 30
 
 // Initial Code
 
+var yell = function() {
+	console.log('Princess! Come with me! Run!')
+};
 
+var runRight = function() {
+	movement: 10
+};
+
+var waddleRight = function() {
+	movement: 1
+}
 
 
 
